4f28bd62d04c1f917e21893b7c64777ebd2e80b6
Code With Stein <codewithstein@Steins-MBP.home>
2020-09-19 08:42:04 +0200

    part 17

--- apps/userprofile/forms.py
+++ apps/userprofile/forms.py
@@ -7,6 +7,16 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(max_length=255, required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'input'
+        self.fields['email'].widget.attrs['class'] = 'input'
+        self.fields['password1'].widget.attrs['class'] = 'input'
+        self.fields['password2'].widget.attrs['class'] = 'input'
+        self.fields['first_name'].widget.attrs['class'] = 'input'
+        self.fields['last_name'].widget.attrs['class'] = 'input'
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
