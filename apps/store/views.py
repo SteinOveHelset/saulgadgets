8d66a94740dbccb1de36a881affb4f7112ce1cd6
Code With Stein <codewithstein@Steins-MBP.home>
2020-10-14 06:33:03 +0200

    part 26

--- apps/store/views.py
+++ apps/store/views.py
@@ -9,11 +9,22 @@ from .models import Product, Category, ProductReview
 
 def search(request):
     query = request.GET.get('query')
-    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    instock = request.GET.get('instock')
+    price_from = request.GET.get('price_from', 0)
+    price_to = request.GET.get('price_to', 100000)
+    sorting = request.GET.get('sorting', '-date_added')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
+
+    if instock:
+        products = products.filter(num_available__gte=1)
 
     context = {
         'query': query,
-        'products': products
+        'products': products.order_by(sorting),
+        'instock': instock,
+        'price_from': price_from,
+        'price_to': price_to,
+        'sorting': sorting
     }
 
     return render(request, 'search.html', context)
