c2d8412687614334dddea89710c99f032cab52e0
manos <manos.mark@gmail.com>
2021-01-23 20:10:46 +0200

    deactivate coupons when they are not available

--- apps/coupon/models.py
+++ apps/coupon/models.py
@@ -16,7 +16,7 @@ class Coupon(models.Model):
         if self.active == False:
             is_active = False
         
-        if self.num_used >= self.num_available and self.num_available != 0:
+        if self.num_used < self.num_available or self.num_available == 0:
             is_active = False
         
         return is_active
